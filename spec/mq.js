0bfad7c3c5cbdb8009604da11ccce9401402f053
Ming Liu <vmliu1@gmail.com>
2014-06-01 19:35:02 -0700

    finally finished all the mq tests

--- spec/mq.js
+++ spec/mq.js
@@ -1,4 +1,8 @@
 
+var fork = require('child_process').fork
+  , path = require('path')
+  , Emitter = require('events').EventEmitter
+
 var chai = require('chai')
 var sinon = require('sinon')
 var Promise = require('promise')
@@ -8,6 +12,31 @@ chai.should()
 chai.use(require('sinon-chai'))
 
 var ready = require('../lib/mq')
+  , handshake = require('../lib/handshake/pool')
+  , fixture = path.join(__dirname, './fixture/worker.js')
+
+function makeFixtureProcess(onReady) { return handshake(fork(fixture), onReady) }
+
+function mockHandShake(onWorkerReady, onPoolReady) {
+  var worker = new Emitter
+
+  worker.pid = process.pid
+  worker.send = function(message) { process.emit('message', message) }
+
+  onPoolReady = onPoolReady || function onPoolReady(worker) {
+    worker.send('job')
+  }
+
+  handshake(worker, onPoolReady)
+
+  // TODO this is binding send
+  // when to unbind it?
+  process.send = function(message) {
+    worker.emit('message', message)
+  }
+
+  ready(onWorkerReady)
+}
 
 describe('Message Queue', function() {
   it('should export a single function that takes a single param', function() {
@@ -15,192 +44,199 @@ describe('Message Queue', function() {
     ready.length.should.equal(1)
   })
 
+  describe('Handshake', function() {
+    it('should complete handshake before starting job', function(done) {
+      makeFixtureProcess(function(worker) {
+        done()
+
+        worker.kill()
+      })
+    })
+  })
+
   describe('Message Queue supports 3 call styles', function() {
+    afterEach(function() {
+      process.send = null
+      process.removeAllListeners()
+    })
+
     describe('callback: invoke callback function with signature (err, data) when done', function() {
       it('should pass data back', sinon.test(function(done) {
-        var addListener = this.stub(process, 'on').callsArg(1)
-          , clock = this.clock
-          , send
+        var context = this
+          , clock = context.clock
+          , setImmediate = clock.setImmediate.bind(clock)
+          , tick = clock.tick.bind(clock)
 
-        process.send = function() {
-          if (done) { done(); done = null }
-        }
-
-        send = this.spy(process, 'send')
-
-        ready(function(job, callback) {
+        mockHandShake(function onWorkerReady(job, callback) {
           setImmediate(function() {
+            var send = context.spy(process, 'send')
+
             callback(null, 'payload')
 
             send.should.be.calledTwice
             send.firstCall.should.be.calledWith('data')
             send.secondCall.should.be.calledWith('payload')
 
-            process.send = null
+            send.restore()
+
+            done()
           })
 
-          clock.tick(10)
+          tick(10)
         })
-
-        addListener.should.be.calledOnce
-        addListener.should.be.calledWith('message', sinon.match.typeOf('function'))
       }))
 
       it('should take err as fatal signal', sinon.test(function(done) {
-        var addListener = this.stub(process, 'on').callsArg(1)
-          , clock = this.clock
-          , send
+        var context = this
+          , clock = context.clock
+          , setImmediate = clock.setImmediate.bind(clock)
+          , tick = clock.tick.bind(clock)
 
-        process.send = function() {
-          if (done) { done(); done = null }
-        }
-
-        send = this.spy(process, 'send')
-
-        ready(function(job, callback) {
+        mockHandShake(function onWorkerReady(job, callback) {
           setImmediate(function() {
+            var send = context.spy(process, 'send')
+
             callback('errord')
 
             send.should.be.calledTwice
             send.firstCall.should.be.calledWith('error')
             send.secondCall.should.be.calledWith('errord')
 
-            process.send = null
+            send.restore()
+
+            done()
           })
 
-          clock.tick(10)
+          tick(10)
         })
-
-        addListener.should.be.calledOnce
-        addListener.should.be.calledWith('message', sinon.match.typeOf('function'))
       }))
     })
 
     describe('thunk: return thunk with signature function(function(err, data) {}), {}', function() {
       it('should pass data back via thunk', sinon.test(function(done) {
-        var addListener = this.stub(process, 'on').callsArg(1)
-          , clock = this.clock
-          , send
+        var context = this
+          , clock = context.clock
+          , setImmediate = clock.setImmediate.bind(clock)
+          , tick = clock.tick.bind(clock)
 
-        process.send = function() {
-          if (done) { done(); done = null }
-        }
-
-        send = this.spy(process, 'send')
-
-        ready(function(job) {
+        mockHandShake(function onWorkerReady(job) {
           return function(callback) {
             setImmediate(function() {
+              var send = context.spy(process, 'send')
+
               callback(null, 'payload')
 
               send.should.be.calledTwice
               send.firstCall.should.be.calledWith('data')
               send.secondCall.should.be.calledWith('payload')
 
-              process.send = null
+              done()
             })
-
-            clock.tick(10)
           }
         })
 
-        addListener.should.be.calledOnce
-        addListener.should.be.calledWith('message', sinon.match.typeOf('function'))
+        tick(10)
       }))
 
       it('should take err as fatal signal', sinon.test(function(done) {
-        var addListener = this.stub(process, 'on').callsArg(1)
-          , clock = this.clock
-          , send
+        var context = this
+          , clock = context.clock
+          , setImmediate = clock.setImmediate.bind(clock)
+          , tick = clock.tick.bind(clock)
 
-        process.send = function() {
-          if (done) { done(); done = null }
-        }
-
-        send = this.spy(process, 'send')
-
-        ready(function(job) {
+        mockHandShake(function onWorkerReady(job) {
           return function(callback) {
             setImmediate(function() {
+              var send = context.spy(process, 'send')
+
               callback('errord')
 
               send.should.be.calledTwice
               send.firstCall.should.be.calledWith('error')
               send.secondCall.should.be.calledWith('errord')
 
-              process.send = null
+              done()
             })
 
-            clock.tick(10)
+            tick(10)
           }
         })
-
-        addListener.should.be.calledOnce
-        addListener.should.be.calledWith('message', sinon.match.typeOf('function'))
       }))
     })
 
     describe('promise: return a `thenable` of signature { then: function(onFulfilled, onRejected) {} }', function() {
-      it('should pass data back via thunk', sinon.test(function(done) {
-        var addListener = this.stub(process, 'on').callsArg(1)
-          , clock = this.clock
-          , send
+      it('should pass data back via promise', sinon.test(function(done) {
+        var context = this
+          , clock = context.clock
+          , setImmediate = clock.setImmediate.bind(clock)
+          , tick = clock.tick.bind(clock)
 
-        process.send = function() {
-          if (done) { done(); done = null }
-        }
+        mockHandShake(onWorkerReady, onPoolReady)
 
-        send = this.spy(process, 'send')
+        function onPoolReady(worker) {
+          var send = sinon.spy(process, 'send')
 
-        ready(function(job) {
-          return new Promise(function(resolve, reject) {
-            setImmediate(function() {
-              resolve('payload')
-
+          worker.once('message', function getType(type) {
+            worker.once('message', function getPayload(data) {
               send.should.be.calledTwice
               send.firstCall.should.be.calledWith('data')
               send.secondCall.should.be.calledWith('payload')
 
-              process.send = null
+              send.restore()
+
+              done()
+            })
+          })
+
+          worker.send('job')
+        }
+
+        function onWorkerReady(job) {
+          return new Promise(function(resolve, reject) {
+            setImmediate(function() {
+              resolve('payload')
             })
 
-            clock.tick(10)
+            tick(10)
           })
-        })
-
-        addListener.should.be.calledOnce
-        addListener.should.be.calledWith('message', sinon.match.typeOf('function'))
+        }
       }))
 
       it('should take err as fatal signal', sinon.test(function(done) {
-        var addListener = this.stub(process, 'on').callsArg(1)
-          , clock = this.clock
-          , send
+        var context = this
+          , clock = context.clock
+          , setImmediate = clock.setImmediate.bind(clock)
+          , tick = clock.tick.bind(clock)
 
-        process.send = function() {
-          if (done) { done(); done = null }
-        }
+        mockHandShake(onWorkerReady, onPoolReady)
 
-        send = this.spy(process, 'send')
+        function onPoolReady(worker) {
+          var send = sinon.spy(process, 'send')
 
-        ready(function(job) {
-          return new Promise(function(resolve, reject) {
-            setImmediate(function() {
-              reject('errord')
-
+          worker.once('message', function getType(type) {
+            worker.once('message', function getPayload(data) {
               send.should.be.calledTwice
               send.firstCall.should.be.calledWith('error')
               send.secondCall.should.be.calledWith('errord')
 
-              process.send = null
+              send.restore()
+
+              done()
+            })
+          })
+
+          worker.send('job')
+        }
+
+        function onWorkerReady(job) {
+          return new Promise(function(resolve, reject) {
+            setImmediate(function() {
+              reject('errord')
             })
 
-            clock.tick(10)
+            tick(10)
           })
-        })
-
-        addListener.should.be.calledOnce
-        addListener.should.be.calledWith('message', sinon.match.typeOf('function'))
+        }
       }))
     })
   })
